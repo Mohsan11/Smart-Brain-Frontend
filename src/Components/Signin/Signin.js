a1335ea2e0d57911d510ac9d80ceefbd896b1b2f
Mohsan Ali <mohsanali1132@gmail.com>
2022-07-25 01:39:05 +0500

    added full functionality to signin component of using datbase for ligin in

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -1,56 +1,90 @@
 import React from "react";
-const Signin = ({ onRouteChange }) => {
-  return (
-    <div>
-      <article className="br3 shadow-5 b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
-        <main className="pa4 black-80 ">
-          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f4 fw6 ph0 mh0">Sign In</legend>
-            <div className="mt3">
-              <label className="db fw6 lh-copy f6" email="email-address">
-                Email
-              </label>
+class Signin extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      signinEmail: "",
+      signinPassword: "",
+    };
+  }
+  onEmailChange = (event) => {
+    this.setState({ signinEmail: event.target.value });
+  };
+  onPasswordChange = (event) => {
+    this.setState({ signinPassword: event.target.value });
+  };
+  onSubmitSignin = () => {
+    fetch("http://localhost:3001/signin", {
+      method: "post",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        email: this.state.signinEmail,
+        password: this.state.signinPassword,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data === "success") {
+          this.props.onRouteChange("home");
+        }
+      });
+  };
+  render() {
+    const { onRouteChange } = this.props;
+    return (
+      <div>
+        <article className="br3 shadow-5 b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
+          <main className="pa4 black-80 ">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" email="email-address">
+                  Email
+                </label>
+                <input
+                  onChange={this.onEmailChange}
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address4"
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" email="password">
+                  Password
+                </label>
+                <input
+                  onChange={this.onPasswordChange}
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password3"
+                />
+              </div>
+            </fieldset>
+            <div className="">
               <input
-                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                type="email"
-                name="email-address"
-                id="email-address4"
+                onClick={() => {
+                  this.onSubmitSignin();
+                }}
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
+                value="Sign in"
               />
+              <p
+                onClick={() => {
+                  onRouteChange("register");
+                }}
+                className="f4 link dim black pointer underline db"
+              >
+                Register
+              </p>
             </div>
-            <div className="mv3">
-              <label className="db fw6 lh-copy f6" email="password">
-                Password
-              </label>
-              <input
-                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                type="password"
-                name="password"
-                id="password3"
-              />
-            </div>
-          </fieldset>
-          <div className="">
-            <input
-              onClick={() => {
-                onRouteChange("home");
-              }}
-              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-              type="submit"
-              value="Sign in"
-            />
-            <p
-              onClick={() => {
-                onRouteChange("register");
-              }}
-              className="f4 link dim black pointer underline db"
-            >
-              Register
-            </p>
-          </div>
-          <div className="lh-copy mt3"></div>
-        </main>
-      </article>
-    </div>
-  );
-};
+            <div className="lh-copy mt3"></div>
+          </main>
+        </article>
+      </div>
+    );
+  }
+}
 export default Signin;
